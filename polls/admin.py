203ea4fabf6d51a6b2be6e9e93b3278e8e69e584
phongcubu <thanhphongdz2707@gmail.com>
2022-10-02 10:48:57 +0700

    done view-tenples-models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question,Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ( "question_text" ,"pub_date")
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ( "question" ,"choice_text","votes")
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice,ChoiceAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
-# Create your views here.
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
+
+def detail(request, question_id):
+    #cách 1
+    try:
+        question = Question.objects.get(pk=question_id)
+        list_choice = question.taichoices.all()
+        
+
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    # cách 2 
+    # question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question, "tailistchoice" :list_choice})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
