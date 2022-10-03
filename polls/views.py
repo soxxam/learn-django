8371cc4b55d1362487948bec0fd8d9135dbc36e0
TaiTau <manbatai2000@gmail.com>
2022-10-04 00:03:57 +0700

    'hoanthanh'

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import Http404
-
-
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse,HttpResponseRedirect,Http404
+from django.urls import reverse
 from .models import Question
 
 
@@ -15,19 +13,28 @@ def detail(request, question_id):
     #cách 1
     try:
         question = Question.objects.get(pk=question_id)
-        list_choice = question.taichoices.all()
-        
-
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
 
     # cách 2 
     # question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'detail.html', {'question': question, "tailistchoice" :list_choice})
+    return render(request, 'detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'templates/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
